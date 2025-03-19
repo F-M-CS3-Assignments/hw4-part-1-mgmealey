a32a618a3ec282883c94b4bf68912bd3b748488c
Madeline Mealey <madelinemealey@Madelines-MacBook-Air-10.local>
2025-03-19 12:30:24 -0400

    sort function and to string functions

--- bdc.h
+++ bdc.h
@@ -1,3 +1,5 @@
+#ifndef BDC_H
+#define BDC_H
 
 #include <vector>
 #include <string>
@@ -8,4 +10,8 @@ using namespace std;
 // for one or both of these methods, it is not a substantial difference
 // since the vectors used in this program are not very large.
 string vec_to_string(vector<int> v);
+vector<int> sort(vector<int> input);
 vector<int> biggest_divisible_conglomerate(vector<int> input);
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -5,9 +5,67 @@
 #include <cassert>
 #include <iostream>
 #include <set>
+#include <string>
+
+using namespace std;
+
+//turns vector into string to be outputted
+string vec_to_string(vector<int> v){
+	string output;
+	output = output + "[";
+	for (int i =  0; i < v.size() - 1; ++i){
+		output = output + to_string(v[i]) + ", ";
+	}
+	output = output + to_string(v[v.size() - 1]) + "]";
+
+	return output;
+}
+
+vector<int> sort(vector<int> input){
+
+	vector<int> sorted_vec;
+	int start = 0;
+
+	while (!input.empty()){
+		//first find smallest number in sequence
+		int min_idx = 0;
+		int min = input[0];
+
+		for(time_t i = 0; i < input.size(); ++i){
+			if (input[i] < min){
+				min = input[i];
+				min_idx = i;
+			}
+		}
+		
+		//add min number to new sorted vector and then remove it from the original vector
+		sorted_vec.push_back(min);
+		input.erase(input.begin() + min_idx);
+
+	}
+	return sorted_vec;
+}
+
+vector<int> biggest_divisible_conglomerate(vector<int> input){
+	vector<int> sorted_vector = sort(input);
+	return sorted_vector;
+}
+
+
+void tests(){
+	//test sort
+	cout << "Testing sort" << endl;
+	vector<int> values = {28, 22, 7, 2, 8, 14, 24, 56};
+	vector<int> sorted_vec = sort(values);
+	assert(vec_to_string(sorted_vec) == "[2, 7, 8, 14, 22, 24, 28, 56]");
+	cout << "tests passed!" << endl;
+	
+}
 
 
 int main() {
+
+	tests();
 
 	// Test 1 (example from assignment description)
 	vector<int> values = {28, 22, 7, 2, 8, 14, 24, 56};
