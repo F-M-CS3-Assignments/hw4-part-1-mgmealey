701fa06f1484583a18f749f38393ac0a97f2ef4a
Madeline Mealey <madelinemealey@Madelines-MacBook-Air-10.local>
2025-03-21 18:38:28 -0400

    finished code

--- bdc.h
+++ bdc.h
@@ -9,9 +9,18 @@ using namespace std;
 // Although pass by reference would probably be more efficient
 // for one or both of these methods, it is not a substantial difference
 // since the vectors used in this program are not very large.
-string vec_to_string(vector<int> v);
-vector<int> sort(vector<int> input);
-vector<int> biggest_divisible_conglomerate(vector<int> input);
+string vec_to_string(const vector<int>& v);
 
+int find_next_dividend(const vector<int>& input, int start);
+
+vector<int> sub_vec(vector<int> vec, int start);
+
+vector<int> longest_vector(const vector<vector<int>>& candidates);
+
+vector<int> combine(vector<int>& l, vector<int>& r);
+
+vector<int> bdc_helper(vector<int>& input);
+
+vector<int> biggest_divisible_conglomerate(vector<int>& input);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 #include <set>
 #include <string>
 
+#define NPOS UINT32_MAX
+
 using namespace std;
 
 //turns vector into string to be outputted
-string vec_to_string(vector<int> v){
+string vec_to_string(const vector<int>& v){
 	string output;
 	output = output + "[";
 	for (int i =  0; i < v.size() - 1; ++i){
@@ -48,24 +50,27 @@ vector<int> sort(vector<int> input){
 
 //From the position i+1, find the position of next number in the input vector which
 //is divisible by input[i]
-int find_next_dividend(vector<int> input, int start){
-	for (int i = (start  + 1); i < input.size(); ++i){
-		if (input[i] % start == 0){
+int find_next_dividend(const vector<int>& input, int start){
+	for (int i = start; i < input.size(); ++i){
+		//cout << input[i] << " % " << input[start] << " = " << input[i] % input[start] << endl;
+		if (input[i] % input[start - 1] == 0){
 			return i;
 		}
 	}
 	return -1;
 }
 
-vector<int> sub_vec(vector<int> vec, int start){
+vector<int> sub_vec(vector<int> input, int start){
 	vector<int> answer;
-	for (int i = start; i < vec.size(); ++i){
-		answer.push_back(vec[i]);
-	}	
+	if (start > 0){
+		for (int i = start; i < input.size(); i++){
+			answer.push_back(input[i]);
+		}
+	}
 	return answer;
 }
 
-vector<int> longest_vector(vector<vector<int>> candidates){
+vector<int> longest_vector(const vector<vector<int>>& candidates){
 	
 	int max_length = candidates[0].size();
 	int idx_longest = 0;
@@ -79,62 +84,78 @@ vector<int> longest_vector(vector<vector<int>> candidates){
 	return candidates[idx_longest];
 }
 
-vector<int> combine(vector<int> l, vector<int> r){
-	vector<int> combined;
-	for (int i = 0; i < l.size(); ++i){
-		combined.push_back(l[i]);
+vector<int> combine(vector<int>& l, vector<int>& r){
+
+	if(r.size() > 0 && r.at(0) % l.at(0) != 0){
+		return l;
 	}
-	for(int i = 0; i < r.size(); ++i){
-		combined.push_back(r[i]);
+	
+	vector<int> output;
+
+	for(size_t i = 0; i < l.size(); i++){
+		output.push_back(l.at(i));
 	}
 
-	return combined;
+	for(size_t i = 0; i < r.size(); i++){
+		output.push_back(r.at(i));
+	}
+
+	return output;
 }
 
-vector<int> bdc_helper(vector<int> input){
+vector<int> bdc_helper(vector<int>& input){
 	if (input.size() == 1 || input.size() == 0){
 		return input;
 	}
 
 	vector<vector<int>> candidates;
 
-	for (int i = 0; i < input.size() - 1; ++i){
+	for (size_t i = 0; i < input.size(); i++){
 		vector<int> l;
 		l.push_back(input[i]);
-		int j = find_next_dividend(input, (i + 1));
+		int next_div = find_next_dividend(input, (i + 1));
 
-		vector<int> rin = sub_vec(input, j);
+		if (next_div == -1){
+			candidates.push_back(l);
+			continue;
+		}
+
+		vector<int> rin = sub_vec(input, next_div);
+
 		vector<int> r = bdc_helper(rin);
+
 		vector<int> cand_v = combine(l, r);
+
 		candidates.push_back(cand_v);
 	}
 
 	return longest_vector(candidates);
 }
 
-vector<int> biggest_divisible_conglomerate(vector<int> input){
-	vector<int> sorted_vector = sort(input);
-	return sorted_vector;
+vector<int> biggest_divisible_conglomerate(vector<int>& input){
+	vector<int> sorted = sort(input);
+	return bdc_helper(sorted);
 }
 
-void tests(){
-	//test sort
-	cout << "Testing sort" << endl;
-	vector<int> values = {28, 22, 7, 2, 8, 14, 24, 56};
-	vector<int> sorted_vec = sort(values);
-	assert(vec_to_string(sorted_vec) == "[2, 7, 8, 14, 22, 24, 28, 56]");
-	cout << "tests passed!" << endl;
+// void tests(){
+// 	// //test sort
+// 	// cout << "Testing sort" << endl;
+// 	vector<int> values = {28, 22, 7, 2, 8, 14, 24, 56};
+// 	sort(values.begin(), values.end(), greater<int>());
+
+// 	assert(vec_to_string(values) == "[56, 28, 24, 22, 14, 8, 7, 2]");
 	
 
-	cout << "testing find dividend" << endl;
-	int next_divident = find_next_dividend(sorted_vec, 0);
-	assert(next_divident == 8);
-}
+// 	cout << "testing find dividend" << endl;
+// 	int next_divident = find_next_dividend(values, 0);
+// 	cout << next_divident << endl;
+// 	assert(next_divident == 1);
+// }
 
 
 int main() {
 
-	tests();
+	// tests();
 
 	// Test 1 (example from assignment description)
 	vector<int> values = {28, 22, 7, 2, 8, 14, 24, 56};
